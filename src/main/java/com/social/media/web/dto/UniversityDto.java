c1a455c0369c63d9dfe558a9031fc80ac61fb2da
akshayjagalpure <akshayjagalpure333@gmail.com>
2019-03-22 23:45:43 +0530

    Result

--- src/main/java/com/social/media/web/dto/UniversityDto.java
+++ src/main/java/com/social/media/web/dto/UniversityDto.java
@@ -5,6 +5,9 @@ public class UniversityDto {
 	private String branch;
 	private String year;
 	private byte[] syllabus;
+	private String branch1;
+	private String year1;
+	private byte[] result;
 
 	public String getBranch() {
 		return branch;
@@ -29,4 +32,24 @@ public class UniversityDto {
 	public void setSyllabus(byte[] syllabus) {
 		this.syllabus = syllabus;
 	}
+
+	public String getBranch1() {
+		return branch1;
+	}
+
+	public void setBranch1(String branch1) {
+		this.branch1 = branch1;
+	}
+
+	public String getYear1() {
+		return year1;
+	}
+
+	public void setYear1(String year1) {
+		this.year1 = year1;
+	}
+
+	public byte[] getresult(){return result;}
+
+	public void setresult(byte[] result){this.result=result;}
 }
